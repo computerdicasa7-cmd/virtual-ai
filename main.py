ba009dd4f1d40f3d9e6dfb36374a79224407a14f
computerdicasa7-cmd <computerdicasa7@gmail.com>
2026-02-22 21:42:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ import threading
 import time
 import re
 
-app = Flask(name)
+app = Flask(__name__)
 
-TOKEN = "8537405775"
+TOKEN = "8537405775:AAESPuTNwnEAIajnz00P1W6CaDeYo-L3YFs"
 CHAT_ID = "1451965962"
 
 URL = "https://www.snai.it/virtuali/ultimi-risultati/hgcals-5"
@@ -27,6 +27,7 @@ pass
 def analizza_virtuali():
 global ultimo_risultato
 
+```
 while True:
     try:
         r = requests.get(URL, headers={"User-Agent": "Mozilla/5.0"})
@@ -44,9 +45,9 @@ while True:
                 gol = int(ultimo[0]) + int(ultimo[2])
 
                 if gol <= 2:
-                    previsione = "OVER 2.5 nella prossima corsa"
+                    previsione = "GIOCA OVER 2.5 nella prossima corsa"
                 else:
-                    previsione = "UNDER 3.5 nella prossima corsa"
+                    previsione = "GIOCA UNDER 3.5 nella prossima corsa"
 
                 manda_telegram(
                     f"⚽ MITICO FOOTBALL SNAI\n"
@@ -58,6 +59,7 @@ while True:
 
     except:
         time.sleep(60)
+```
 
 @app.route("/")
 def home():
@@ -66,8 +68,6 @@ return "BOT VIRTUAL CALCIO ATTIVO"
 def start_bot():
 analizza_virtuali()
 
-
-
-if **name** == "__main__":
+if __name__ == "__main__":
 threading.Thread(target=start_bot, daemon=True).start()
 app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
