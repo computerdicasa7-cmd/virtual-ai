b621a56224eb50efc8b9d726d8d63d74c96664fd
computerdicasa7-cmd <computerdicasa7@gmail.com>
2026-02-22 10:22:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
+import os
 from flask import Flask
-
 app = Flask(**name**)
-
 @app.route("/")
 def home():
 return "FUNZIONA"
-
 if **name** == "**main**":
 import os
 app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
